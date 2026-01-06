0b892c9269e794c53b6221074c46fcc581007245
Mansur <hmmansur050@gmail.com>
2026-01-06 12:57:46 +0600

    lear width somit

---
+++ js/two.js
@@ -0,0 +1,177 @@
+// let x = 7;
+
+// if( true){
+
+//     let x = 5;
+//  console.log(x);
+
+// }
+// console.log(x);
+
+// carName = "bolbo";
+
+// let carName;
+
+// console.log(carName)
+
+// var x = 4;
+
+// x = 4;
+
+
+// console.log(x);
+
+// let x = 5 + 5;
+
+// let y = "5" + 5;
+
+// let z = "hello" + 5;
+
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof z);
+
+
+
+// let x = 7;
+// let y = 5;
+// let z = 7;
+
+
+
+// console.log(x == z);
+
+// let car = ["mansur", "imran", "manik" ,]
+
+// console.log(car);
+// let x = 5;
+// let y = 6;
+// let z = 7;
+
+// let myObject = {x:5, y:6 ,z:7}
+// console.log(myObject);
+
+
+// let mansur = {robul:"40", imran:"90", manik:"100" }
+
+// console.log(typeof mansur);
+
+// function sleep(name ,time){
+//     console.log(name + " my name is mansur" + time);
+
+// }
+
+// console.log(name);
+// sleep("mansur", " 10:pm")
+// sleep("imran", " 11:pm")
+// sleep("manik", " 12:pm")
+
+// function mansur(mansur,time){
+//     console.log(mansur + " mehidi bhai ekta boka choda " + time);
+
+// }
+
+
+// mansur("mansur", "10:pm")
+// mansur("rakib" , "11:pm")
+// mansur("imran" , "12:pm")
+
+// function myFunction(a , b){
+//     return a * b
+
+// }
+// let x = myFunction(3 , 4);
+
+// console.log(x);
+
+// function myprojct( d ,n){
+//     return d * n
+// }
+// let v = myprojct( 4,5)
+// console.log(v);
+//  var carName = "bmw"
+
+//  function myFunction(){
+//      let carName = "volvo";   
+//      console.log(carName);
+
+// }
+//  myFunction()
+//  console.log(carName);
+
+// let x = "6" + "hfjf"
+// console.log(x);
+//  let name = "mansur";
+//  let boyse = 22;
+//  let aeg = "pacha";
+//  let color = "blue";
+
+//  let myObject = {name:"mansur" , boyse:22,aeg:"pacha",color: "blue" }
+
+//  console.log(typeof boyse);
+//   let x =""
+
+//   console.log(x);
+
+//  function sleep(name ,time){
+//     console.log(name +"mansur gumay 10:pm por jnto" + time);
+
+//  }
+//  sleep("mansue"," 12 pm");
+//  sleep("manik"," 1 pm");
+//  sleep("imran"," 7 pm");
+/////////////////// functionpart
+
+//  console.log("my name is mansur");
+
+
+
+// function myFunction(a,b){
+//     console.log("hello");
+//     return a * b;
+// }
+// let x = myFunction(4 ,5)
+
+// console.log(x);
+
+// let carName = "mansur"
+
+//  function myFunction(){
+//     var carName = "volvo"
+//     console.log(carName);
+//  }
+//  myFunction();
+
+//  console.log(carName)
+
+
+// const cat = {
+//   mane: "bmw",
+//   kg: 40,
+//   porichoy: "bangladesh",
+//   malik: "mansur",
+//   start: function () {
+//     this.drive();
+//     console.log("car is start")
+//   },
+//   drive: function () {
+//     console.log("car hase drivng")
+//   },
+
+// };
+// console.log(cat.kg)
+// console.log(cat["malik"])
+
+// cat.start();
+
+// let x = 5; //litrel sintext
+
+
+// let y = new Number(5);
+
+// console.log(y)
+
+
+let text = "AMR BASA PATUAKHLI BARISHAL /mansur/ bauphal";
+
+console.log(text);
